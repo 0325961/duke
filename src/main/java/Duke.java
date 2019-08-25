577d05c6b56b5705fef5f6cbadc08a6f5a82e41c
Michelle Toh <michelletoh@Michelles-MacBook-Pro.local>
2019-08-26 01:01:15 +0800

    level 3 (A-classes) DONE

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,9 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+
     public static void level1(String userInput) {
         Scanner input = new Scanner(System.in);
         while (true) {
@@ -81,7 +83,40 @@ public class Duke {
 
     }
 
+    public static void level3WithUseOfClass(String userInput) {
+        ArrayList<Task> arrList = new ArrayList<Task>();
+        Scanner input = new Scanner(System.in);
 
+        while(true) {
+            String[] splitted = userInput.split(" ", 2);
+
+            if (splitted[0].equals("done")) {
+                int index = Integer.parseInt(splitted[1]) - 1;
+                //update status
+                arrList.get(index).TaskDone();
+                System.out.println("Nice! I've marked this task as done: ");
+                // PRINT OUT THE DONE TASK : System.out.println("[" + )
+                System.out.println("[" + arrList.get(index).getStatusIcon() + "]" + arrList.get(index).getDescription());
+            }
+            else if (userInput.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            else if (userInput.equals("list")) {
+                //print out the list
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < arrList.size(); i ++) {
+                    System.out.println((i+1) + ".[" + arrList.get(i).getStatusIcon() + "]" + arrList.get(i).getDescription());
+                }
+            }
+            else {
+                Task tempTask = new Task(userInput);
+                arrList.add(tempTask);
+                System.out.println("added: " + userInput);
+            }
+            userInput = input.nextLine();
+        }
+    }
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -92,18 +127,22 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
 
+
         Scanner input = new Scanner(System.in);
         String value = input.nextLine();
-        level3(value);
+
+        //level1(value);
+        //level2(value);
+        //level3(value);
+        level3WithUseOfClass(value);
     }
 }
 
 
 
 
-/* METHOD 1 for level 2
-
-METHOD 2 for level 2
+/*
+METHOD 2 (use of array instead of ArrayList) for level 2
         Scanner input = new Scanner(System.in);
 
         boolean toContinue = true;
@@ -127,6 +166,7 @@ METHOD 2 for level 2
                 i += 1;
             }
         }
+
 */
 
 
